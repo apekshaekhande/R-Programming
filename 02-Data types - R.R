116c051f161fc306f8ee26d6dcdef368e07270b1
Apeksha Ekhande <113659344+apekshaekhande@users.noreply.github.com>
2023-01-08 13:50:56 +0530

    Update 02-Data types - R.R

--- 02-Data types - R.R
+++ 02-Data types - R.R
@@ -1,23 +1,23 @@
-#data types
-# Following are the Datatypes in are
-# 1)Vector
-# 2)List
-# 3)Matrix
-# 4)Array
-# 5)Factor
-# 6)Data Frame
+#Data types
+# Following are the Datatypes in R
+            # 1)Vector
+            # 2)List
+            # 3)Matrix
+            # 4)Array
+            # 5)Factor
+            # 6)Data Frame
 
 # 1) Vector - contains elements of same type or class
-# To combine the list of items to a vector, use the c() Function and seperate the items by a comma.
-# Vector - basically of Two types 1)Atomic vector 2)Lists
+   # To combine the list of items to a vector, use the c() Function and seperate the items by a comma.
+   # Vector - basically of Two types 1)Atomic vector 2)Lists
 
 
-# there are basically 5 types of Atomic vectors 
-# i)logical
-# ii)Numaric
-# iii)Character
-# iv)integer
-# v)Complex
+   # there are basically 5 types of Atomic vectors 
+        # i)logical
+        # ii)Numaric
+        # iii)Character
+        # iv)integer
+        # v)Complex
 
 
 # i)Logical-
@@ -128,4 +128,3 @@ m = data.frame(gender = c("Male", "Male","Female"), height = c(152, 171.5, 165),
                 weight = c(81,93,67), Age = c(42,38,64))
 class(m)
 m
-
